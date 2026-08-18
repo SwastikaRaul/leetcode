9e4f508bf452280d6a1f84c4f81ab00637727357
SwastikaRaul <raulswastika20@gmail.com>
2026-08-19 00:13:30 +0530

    Time: 1 ms (24.11%), Space: 19.1 MB (89.13%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n<= 0:
+            return False
+        
+        if(n & (n-1))==0 and (n & 0x55555555) != 0:
+            return True
+        
+        return False
