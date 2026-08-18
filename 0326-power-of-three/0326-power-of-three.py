898468425bfd113a3d39c36295f672cc6e7b2a34
SwastikaRaul <raulswastika20@gmail.com>
2026-08-19 00:03:56 +0530

    Time: 8 ms (59.98%), Space: 19.4 MB (6.63%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n<= 0:
+            return False
+        
+        if n>0 and 1162261467 % n ==0:
+            return True
+        
+        return False
